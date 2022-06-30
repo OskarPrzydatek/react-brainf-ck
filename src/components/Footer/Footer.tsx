ace85a55b5dda2152722349121ea3be517a19456
OMKP <oprzydatek@sii.pl>
2022-06-30 22:10:17 +0200

    sign footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h3 className="footer__author">Brainf*ck Interpreter</h3>
+      <p className="footer__index">Oskar Przydatek S19388</p>
+    </footer>
+  );
+}
